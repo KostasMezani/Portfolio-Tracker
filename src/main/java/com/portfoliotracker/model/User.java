9b924ba3066e7d1c9af68b0785ef311468f6d430
KostasMezani <kotsos.mezanis@gmail.com>
2026-03-25 11:15:09 +0200

    feat: implement User domain model

--- src/main/java/com/portfoliotracker/model/User.java
+++ src/main/java/com/portfoliotracker/model/User.java
@@ -3,5 +3,73 @@ package com.portfoliotracker.model;
 import java.time.LocalDateTime;
 
 public class User {
+private String id;
+private String username;
+private String email;
+private String passwordHash;
+private LocalDateTime createdAt;
 
+//Constructor
+    public User(String id, String username, String email, String passwordHash ,LocalDateTime createdAt) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.createdAt = createdAt;
+    }
+
+    //Empty Constructor(for JSON deserialization)
+    public User(){}
+
+    //Getters & Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
