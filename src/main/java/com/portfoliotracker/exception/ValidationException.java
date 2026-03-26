a4b60e8689d4ff5bd1880b2867bd85828cf0d8c4
KostasMezani <kotsos.mezanis@gmail.com>
2026-03-26 18:30:49 +0200

    feat: implement ValidationException

--- src/main/java/com/portfoliotracker/exception/ValidationException.java
+++ src/main/java/com/portfoliotracker/exception/ValidationException.java
@@ -1,4 +1,7 @@
 package com.portfoliotracker.exception;
 
-public class ValidationException {
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
 }
