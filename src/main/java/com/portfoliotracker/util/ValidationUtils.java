e8e88cb65826a22ebc86478e5362c110e3893205
KostasMezani <kotsos.mezanis@gmail.com>
2026-03-27 20:30:33 +0200

    feat: implement ValidationUtils

--- src/main/java/com/portfoliotracker/util/ValidationUtils.java
+++ src/main/java/com/portfoliotracker/util/ValidationUtils.java
@@ -1,4 +1,20 @@
 package com.portfoliotracker.util;
 
 public class ValidationUtils {
+
+    public static boolean isNotEmpty(String value){
+        return value != null && !value.isBlank();
+    }
+
+    public static boolean isValidPassword(String password){
+        return password!= null && password.length() >= 6;
+    }
+
+    public static boolean isValidEmail(String email){
+       return email != null && email.contains("@");
+    }
+
+    public static boolean passwordsMatch(String p1, String p2) {
+       return p1 != null && p1.equals(p2);
+    }
 }
