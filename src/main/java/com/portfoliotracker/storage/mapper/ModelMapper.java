a58e728db9479e24cf79320b1818e45d82bd27e6
KostasMezanis <kotsos.mezanis@gmail.com>
2026-03-31 21:55:12 +0300

    feat: implement ModelMapper

--- src/main/java/com/portfoliotracker/storage/mapper/ModelMapper.java
+++ src/main/java/com/portfoliotracker/storage/mapper/ModelMapper.java
@@ -1,4 +1,8 @@
 package com.portfoliotracker.storage.mapper;
 
+/**
+ * ModelMapper is not needed in this implementation.\
+ * Gson handles JSON mapping automatically.
+ */
 public class ModelMapper {
 }
